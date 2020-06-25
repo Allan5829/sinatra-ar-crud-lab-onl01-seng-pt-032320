2ecbc6dbef2c93498ec2349a4f64e46ec215c80f
Allan Farca <allan.farca@gmail.com>
2020-06-25 08:24:00 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -23,7 +23,7 @@ class ApplicationController < Sinatra::Base
   post '/articles' do
     @article = Article.create(params)
 
-    redirect "/articles/#{params[:id]}"
+    redirect "/articles/#{params[id]}"
   end
 
   get '/articles/:id' do
